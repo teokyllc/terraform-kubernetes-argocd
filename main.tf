867d76a3ab1d9cd47565610a89a14ae27ca332ca
Allan Taylor <allan@startmail.com>
2021-10-12 07:06:48 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,73 +21,7 @@ resource "null_resource" "install_argocd" {
   }
 }
 
-resource "null_resource" "add_argocd_config_map" { 
-  depends_on = [null_resource.install_argocd]  
-  provisioner "local-exec" { 
-    command = <<-EOT
-      cat <<EOF | kubectl apply -f -
-      apiVersion: v1
-      kind: ConfigMap
-      metadata:
-        name: argocd-cm
-        namespace: argocd
-        labels:
-          app.kubernetes.io/name: argocd-cm
-          app.kubernetes.io/part-of: argocd
-      data:
-        configManagementPlugins: |
-          - name: argocd-vault-plugin
-            generate:
-              command: ["argocd-vault-plugin"]
-              args: ["generate", "./"]
-          - name: argocd-vault-plugin-helm
-            generate:
-              command: ["sh", "-c"]
-              args: ["helm template . > all.yaml && argocd-vault-plugin generate all.yaml"]
-          - name: argocd-vault-plugin-kustomize
-            generate:
-              command: ["sh", "-c"]
-              args: ["kustomize build . > all.yaml && argocd-vault-plugin generate all.yaml"]
-        url: https://${var.argo_fqdn}
-        users.anonymous.enabled: ${var.argo_anonymous_users_enabled}
-        users.session.duration: "2h"
-        dex.config: |
-          logger:
-            level: debug
-            format: json
-          connectors:
-          - type: saml
-            id: saml
-            name: Azure AD SAML
-            config:
-              entityIssuer: https://${var.argo_fqdn}/api/dex/callback
-              ssoURL: ${var.argo_sso_login_url}
-              caData: ${var.argo_sso_certificate}
-              redirectURI: https://${var.argo_fqdn}/api/dex/callback
-              usernameAttr: email
-              emailAttr: email
-              groupsAttr: Group
-        resource.customizations.ignoreDifferences.admissionregistration.k8s.io_MutatingWebhookConfiguration: |
-          jsonPointers:
-          - /webhooks/0/clientConfig/caBundle
-          jqPathExpressions:
-          - .webhooks[0].clientConfig.caBundle
-        resource.exclusions: |
-          - apiGroups:
-            - repositories.stash.appscode.com
-            kinds:
-            - Snapshot
-            clusters:
-            - "*.local"
-        resource.compareoptions: |
-          ignoreAggregatedRoles: true
-          ignoreResourceStatusField: crd
-        admin.enabled: ${var.argo_admin_user_enabled}
-        timeout.reconciliation: 180s
-      EOF
-    EOT
-  }
-}
+
 
 #resource "null_resource" "add_argocd_github_app_config_map" { 
 #  depends_on = [null_resource.install_argocd]  
