e5788b51dc16cccdf82ab8ec9b4accfdb8e59dad
Allan Taylor <allan@startmail.com>
2021-10-12 07:45:31 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,60 @@ resource "null_resource" "install_argocd" {
   }
 }
 
-
+resource "null_resource" "install_argocd" { 
+  depends_on = [null_resource.setup_env]  
+  provisioner "local-exec" { 
+    command = <<-EOT
+      cat <<EOF | kubectl apply -f -
+      apiVersion: v1
+      kind: ConfigMap
+      metadata:
+        name: argocd-cm
+        namespace: argocd
+        labels:
+          app.kubernetes.io/name: argocd-cm
+          app.kubernetes.io/part-of: argocd
+      data:
+        url: https://${var.argo_fqdn}/
+        users.anonymous.enabled: "false"
+        users.session.duration: "1h"
+        dex.config: |
+          logger:
+            level: debug
+            format: json
+          connectors:
+          - type: saml
+            id: saml
+            name: Azure AD SAML
+            config:
+              entityIssuer: https://${var.argo_fqdn}/api/dex/callback
+              ssoURL: ${var.argo_sso_login_url}
+              caData: ${var.argo_sso_certificate}
+              redirectURI: https://${var.argo_fqdn}/api/dex/callback
+              usernameAttr: email
+              emailAttr: email
+              groupsAttr: Group
+        resource.customizations.ignoreDifferences.admissionregistration.k8s.io_MutatingWebhookConfiguration: |
+          jsonPointers:
+          - /webhooks/0/clientConfig/caBundle
+          jqPathExpressions:
+          - .webhooks[0].clientConfig.caBundle
+        resource.exclusions: |
+          - apiGroups:
+            - repositories.stash.appscode.com
+            kinds:
+            - Snapshot
+            clusters:
+            - "*.local"
+        resource.compareoptions: |
+          ignoreAggregatedRoles: true
+          ignoreResourceStatusField: crd
+        admin.enabled: "true"
+        timeout.reconciliation: 180s
+      EOF
+    EOT
+  }
+}
 
 #resource "null_resource" "add_argocd_github_app_config_map" { 
 #  depends_on = [null_resource.install_argocd]  
